f04ebda491a07c8b6f188ab836d0a99f6829ec7f
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-27 16:43:29 -0500

    Update getMinOperations.py

--- getMinOperations.py
+++ getMinOperations.py
@@ -3,3 +3,12 @@
 
 class Solution(object):
     def getMinOperations(self, source, target):
+         n = len(source)
+        diff = [target[i] - source[i] for i in range(n)]
+
+        ops = diff[0]
+        for i in range(1, n):
+            if diff[i] > diff[i-1]:
+                ops += diff[i] - diff[i-1]
+
+        return ops
